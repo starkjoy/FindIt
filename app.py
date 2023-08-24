ef9f8a1ca49a187eece55ceb7bd8315e33c44d12
starkjoy <residualcode6@gmail.com>
2023-08-24 21:08:12 +0000

    Restored to working code

--- app.py
+++ app.py
@@ -1,18 +1,10 @@
 from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS
-import mysql.connector
 import os
 from datetime import datetime
 
 app = Flask(__name__)
 cors = CORS(app)
-
-db = mysql.connector.connect(
-    host="localhost",
-    user="FindIt",
-    password="Pa$$w0rd!2023",
-    database="adboard"
-)
 
 @app.route('/')
 def index():
@@ -46,30 +38,6 @@ def upload():
             db.rollback()
             print("Error uploading file:", e)
             return 'Error uploading file'
-
-@app.route('/get_ads', methods=['GET'])
-def get_ads():
-    cursor = db.cursor(dictionary=True)
-    select_query = "SELECT * FROM ads ORDER BY upload_time DESC"
-    cursor.execute(select_query)
-    ads = cursor.fetchall()
-
-    for ad in ads:
-        ad['image_url'] = os.path.join('uploads', os.path.basename(ad['image_url']))
-    return jsonify(ads)
-
-@app.route('/get_uploaded_images', methods=['GET'])
-def get_uploaded_images():
-    uploaded_images = []
-
-    # Get a list of files in the "static" folder
-    static_path = os.path.join(app.root_path, 'static')
-    for filename in os.listdir(static_path):
-        if filename.lower().endswith(('.jpg', '.jpeg', '.png', '.gif')):
-            uploaded_images.append(filename)
-
-    return jsonify(uploaded_images)
-
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,5 @@
 // Wait for the DOM to be fully loaded before executing the code
-document.addEventListener('DOMContentLoaded',  async function()  {
+document.addEventListener('DOMContentLoaded', function()  {
 	// Get reference to the "Add" button (plus icon)
     const addBtn = document.querySelector('.plus-icon');
 	
@@ -28,52 +28,6 @@ document.addEventListener('DOMContentLoaded',  async function()  {
 		// Trigger the click event of the hidden file input element
         fileInput.click();
     });
-    
-	// Function to upload image to the server
-	async function uploadImage(imageFile) {
-		const formData = new FormData();
-		formData.append('file', imageFile);
-		
-		try {
-			const response = await fetch('/upload', {
-				method: 'POST',
-				body: formData
-			});
-			
-			// Check the status of the response
-			if (response.ok) {
-				// Parse the response as text (the relative path)
-				const data = await response.json();
-				
-				// Handle response (show success message, update UI, etc.)
-			} else {
-				// throw an error with the status text
-				throw new Error(response.statusText);
-			}
-        }	
-		catch (error) {
-			// Show error mesage to the user
-			console.error('Error uploading file:', error);
-	    }
-    }
-
-     // Function to fetch uploaded image filenames and create image containers
-     async function fetchAndDisplayImages() {
-        try {
-            const response = await fetch('/get_uploaded_images');
-            const data = await response.json();
-
-            // Create image containers for each uploaded image
-            data.forEach(filename => {
-                createImageContainerFromFilename(filename);
-            });
-        } catch (error) {
-            console.error('Error fetching uploaded images:', error);
-        }
-    }
-
-    // Call the function to fetch and display images when the page loads
-    await fetchAndDisplayImages();
 	
 	// Function to create an image container
     function createImageContainer(imageFile) {
@@ -108,31 +62,6 @@ document.addEventListener('DOMContentLoaded',  async function()  {
 		// Append container to image containers
         imageContainers.appendChild(container);
     }
-
-    function createImageContainerFromFilename(filename) {
-        // Create a new <div> element for the image container
-        const container = document.createElement('div');
-        container.classList.add('image-container');
-    
-        // Create a new <div> element to hold image and content
-        const contentDiv = document.createElement('div');
-        contentDiv.classList.add('container-content');
-    
-        // Create a new <img> element for the image
-        const image = document.createElement('img');
-        image.classList.add('container-image');
-        image.src = `static/${filename}`; // Assuming your images are in the "static" folder
-    
-        // ... your existing code ...
-    
-        // Append content div to main container
-        container.appendChild(contentDiv);
-    
-        const imageContainers = document.querySelector('.image-containers');
-        // Append container to image containers
-        imageContainers.appendChild(container);
-    }
-    
     
 	// Function to get formatted time
     function getFormattedTime() {
